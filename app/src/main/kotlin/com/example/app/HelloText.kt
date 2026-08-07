8ed2e8557975bc279f277691fa42b8e10c847723
DiKo-bu <216696651+DiKo-bu@users.noreply.github.com>
2026-08-07 16:01:24 +0000

    сделал рефакторинг

---
+++ app/src/main/kotlin/com/example/app/HelloText.kt
@@ -0,0 +1,9 @@
+package com.example.app
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HelloText() {
+    Text("Hello, Android!")
+}
---
+++ app/src/main/kotlin/com/example/app/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.example.app
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    HelloText()
+}
