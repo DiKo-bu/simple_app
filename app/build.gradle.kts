7cd31a2476951fc3e4decf2f6c2d0e9924a3f655
DiKo-bu <216696651+DiKo-bu@users.noreply.github.com>
2026-08-07 09:31:37 +0000

    что-то пробую

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,9 +22,8 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2026.08.00"))
+    implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation("androidx.activity:activity-compose")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui:1.8.3")
+    implementation("androidx.compose.material3:material3:1.3.2")
 }
