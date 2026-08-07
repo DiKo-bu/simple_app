7371f9d0e3e3717cc14ad72743b054fe17507ad1
DiKo-bu <216696651+DiKo-bu@users.noreply.github.com>
2026-08-07 09:24:48 +0000

    что-то пробую

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
     kotlin("android") version "2.2.20" apply false
+    kotlin("plugin.compose") version "2.2.20" apply false
 }
