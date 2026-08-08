a060efc42b29a723fc0ed2637ca0f32ba11d332a
DiKo-bu <216696651+DiKo-bu@users.noreply.github.com>
2026-08-08 02:01:01 +0000

    something

--- app/src/main/java/com/example/simple_app/MainActivity.java
+++ app/src/main/java/com/example/simple_app/MainActivity.java
@@ -2,14 +2,34 @@ package com.example.simple_app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.view.ViewGroup;
+import android.view.Gravity;
+
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello World");
-        setContentView(tv);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.setLayoutParams(new ViewGroup.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        TextView left = new TextView(this);
+        left.setText("Слева");
+        left.setGravity(Gravity.START);
+        left.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1)); // вес 1
+
+        TextView right = new TextView(this);
+        right.setText("Справа");
+        right.setGravity(Gravity.END);
+        right.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1)); // вес 1
+
+        layout.addView(left);
+        layout.addView(right);
+        setContentView(layout);
     }
 }
