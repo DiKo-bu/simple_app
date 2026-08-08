bd42ec9ce72326d7809352d0c516920e37c2d7ee
DiKo-bu <216696651+DiKo-bu@users.noreply.github.com>
2026-08-08 02:10:16 +0000

    something

--- app/src/main/java/com/example/simple_app/MainActivity.java
+++ app/src/main/java/com/example/simple_app/MainActivity.java
@@ -6,33 +6,33 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.ViewGroup;
 import android.view.Gravity;
-
+import android.text.Html;   // ← добавил для HTML
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.HORIZONTAL);
         layout.setLayoutParams(new ViewGroup.LayoutParams(
-        ViewGroup.LayoutParams.MATCH_PARENT,
-        ViewGroup.LayoutParams.WRAP_CONTENT));
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
 
         TextView left = new TextView(this);
         left.setText("Слева");
         left.setGravity(Gravity.START);
-        left.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1)); // вес 1
+        left.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
 
         TextView center = new TextView(this);
-        right.setText("<h1>Центр</h1>");
-        right.setGravity(Gravity.CENTER);
-        right.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1)); // вес 1
-        
+        center.setText(Html.fromHtml("<h1>Центр</h1>", Html.FROM_HTML_MODE_LEGACY));
+        center.setGravity(Gravity.CENTER);
+        center.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
+
         TextView right = new TextView(this);
         right.setText("Справа");
         right.setGravity(Gravity.END);
-        right.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1)); // вес 1
-        
+        right.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
 
         layout.addView(left);
         layout.addView(center);
